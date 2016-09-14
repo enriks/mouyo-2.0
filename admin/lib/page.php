77869cfa1f352ecc99c456832d5a19d40819d7bd
enriks <nelo.coto@gmail.com>
2016-09-13 22:00:52 -0600

    lul

--- admin/lib/page.php
+++ admin/lib/page.php
@@ -7,14 +7,11 @@
             $sql="select id_admin,sesion from admin where id_admin=?";
             $params=array(isset($_SESSION['id_admin']));
             $data=Database::getRow($sql,$params);
-            if($data['sesion']==0)
+            if($data['sesion']==0 && isset($_SESSION['id_admin']))
             {
                 header("location: ../main/logout.php");
             }
-            else
-            {
-                $_SESSION['id_admin']=$data['id_admin'];
-            }
+           
             #si se necesita la hora :v
             ini_set("date.timezone","America/El_Salvador");
             $session=false;
@@ -154,88 +151,131 @@
                 }
                 if($_SESSION['permisos']==3)
                 {
-                    $header.="<a href='../main/' class='brand-logo'>
-                            <i class='material-icons left hide-on-med-and-down'>undo</i>Sitio Privado
-		        		</a>
-                        <a href='#' data-activates='mobile' class='button-collapse'><i class='material-icons'>menu</i></a>
-	        					<ul class='right hide-on-med-and-down'>
-		          					<li><a href='../jugos/index.php'>Jugos</a></li>
-		          					<li><a href='../ingredientes/index.php'>ingredientes</a></li>
-		          					<li><a href='../tamanio/index.php'>Vasos</a></li>
-		          					<li><a class='dropdown-button' href='#' data-activates='dropdown'>
-                                    <div class='chip'>
-                                    <img id='foto_perfil imagen_video'
-                                    src='data:image/*;base64,$_SESSION[img_admin]'
-                                    class='responsive-img'>
-                                    $_SESSION[usuario_admin]
-                                  </div></a></li>
-		        				</ul>
-		        				<ul id='dropdown' class='dropdown-content'>
-									<li><a href='../usuario/'>Editar perfil</a></li>
-									<li><a href='../main/logout.php'>Salir</a></li>
-								</ul>
-			        			<ul class='side-nav' id='mobile'>
-	        						<li><a href='../jugos/index.php'>Jugos</a></li>
-		          					<li><a href='../ingredientes/index.php'>ingredientes</a></li>
-		          					<li><a href='../tamanio/index.php'>Vasos</a></li>
-		          					<li><a class='dropdown-button' href='#' data-activates='dropdown'>
-			          				<li><a class='dropdown-button' href='#' data-activates='dropdown-mobile'>$_SESSION[usuario_admin]</a></li>
-	      						</ul>
-	      						<ul id='dropdown-mobile' class='dropdown-content'>
-									<li><a href='../usuario/'>Editar perfil</a></li>
-										<li><a href='../main/logout.php'>Salir</a></li>
-								</ul>";
+                    $header.="<div class='navbar-header'>
+                            <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
+                                <span class='sr-only'>Toggle navigation</span>
+                                <span class='icon-bar'></span>
+                                <span class='icon-bar'></span>
+                                <span class='icon-bar'></span>
+                            </button>
+                            <a class='navbar-brand' href='index.html'>Start Bootstrap</a>
+                        </div>
+                        <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+                <ul class='nav navbar-nav navbar-right'>
+                    <li>
+                        <a href='../jugos/'>Jugos</a>
+                    </li>
+                    <li>
+                        <a href='../ingredientes/'>Ingredientes</a>
+                    </li>
+                    <li class='dropdown'>
+                        <a href='#' class='dropdown-toggle' data-toggle='dropdown'><div class='chip'>
+                              <img src='data:image/*;base64,$_SESSION[img_admin]' alt='Person' width='40' height='40' class='img-circle'>
+                              $_SESSION[usuario_admin]
+                            </div> <b class='caret'></b></a>
+                        <ul class='dropdown-menu'>
+                            <li>
+                                <a href='../profile/'>Editar Perfil</a>
+                            </li>
+                            <li>
+                                <a href='../main/logout.php'>Salir</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>";
                 }
-                elseif($_SESSION['permisos']==4)
+                else
+                {
+                    header("location:../main/denied.php");
+                }
+                if($_SESSION['permisos']==4)
                 {
                     
-                $header.="<a href='../main/' class='brand-logo'>
-                            <i class='material-icons left hide-on-med-and-down'>undo</i>Sitio Privado
-		        		</a>
-                        <a href='#' data-activates='mobile' class='button-collapse'><i class='material-icons'>menu</i></a>
-	        					<ul class='right hide-on-med-and-down'>
-		          					<li><a href='../jugos/index.php'>Jugos</a></li>
-		          					<li><a href='../ingredientes/index.php'>ingredientes</a></li>
-		          					<li><a href='../promociones/index.php'>Promociones</a></li>
-		          					<li><a href='../admin/index.php'>Administradores</a></li>
-		          					<li><a href='../usuario/index.php'>Usuario</a></li>
-		          					<li><a href='../descuentos/index.php'>Descuentos</a></li>
-		          					<li><a href='../tamanio/index.php'>Vasos</a></li>
-		          					<li><a class='dropdown-button' href='#' data-activates='dropdown'><div class='chip'>
-                                    <img id='foto_perfil imagen_video'
-                                    src='data:image/*;base64,$_SESSION[img_admin]'
-                                    class='responsive-img'>
-                                    $_SESSION[usuario_admin]
-                                  </div></a></li>
-		        				</ul>
-		        				<ul id='dropdown' class='dropdown-content'>
-									<li><a href='../usuario/'>Editar perfil</a></li>
-									<li><a href='../main/logout.php'>Salir</a></li>
-								</ul>
-			        			<ul class='side-nav' id='mobile'>
-	        						<li><a href='../jugos/index.php'>Jugos</a></li>
-		          					<li><a href='../ingredientes/index.php'>ingredientes</a></li>
-		          					<li><a href='../promociones/index.php'>Promociones</a></li>
-		          					<li><a href='../admin/index.php'>Administradores</a></li>
-		          					<li><a href='../descuentos/index.php'>Descuentos</a></li>
-		          					<li><a href='../tamanio/index.php'>Vasos</a></li>
-			          				<li><a class='dropdown-button' href='#' data-activates='dropdown-mobile'>$_SESSION[usuario_admin]</a></li>
-	      						</ul>
-	      						<ul id='dropdown-mobile' class='dropdown-content'>
-									<li><a href='../usuario/'>Editar perfil</a></li>
-										<li><a href='../main/logout.php'>Salir</a></li>
-								</ul>";
+                $header.="<div class='navbar-header'>
+                            <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
+                                <span class='sr-only'>Toggle navigation</span>
+                                <span class='icon-bar'></span>
+                                <span class='icon-bar'></span>
+                                <span class='icon-bar'></span>
+                            </button>
+                            <a class='navbar-brand' href='index.html'>Start Bootstrap</a>
+                        </div>
+                        <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+                <ul class='nav navbar-nav navbar-right'>
+                    <li>
+                        <a href='../jugos/'>Jugos</a>
+                    </li>
+                    <li>
+                        <a href='../ingredientes/'>Ingredientes</a>
+                    </li>
+                    <li>
+                        <a href='../descuentos/'>Descuentos</a>
+                    </li>
+                    <li>
+                        <a href='../promociones/'>Promociones</a>
+                    </li> 
+                    <li>
+                        <a href='../admin/'>Administradores</a>
+                    </li>
+                    <li>
+                        <a href='../users/'>Usuarios</a>
+                    </li>
+                    <li>
+                        <a href='../historial/'>Historial</a>
+                    </li>
+                    <li class='dropdown'>
+                        <a href='#' class='dropdown-toggle' data-toggle='dropdown'><div class='chip'>
+                              <img src='data:image/*;base64,$_SESSION[img_admin]' alt='Person' width='40' height='40' class='img-circle'>
+                              $_SESSION[usuario_admin]
+                            </div> <b class='caret'></b></a>
+                        <ul class='dropdown-menu'>
+                            <li>
+                                <a href='../profile/'>Editar Perfil</a>
+                            </li>
+                            <li>
+                                <a href='../main/logout.php'>Salir</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>";
                 }
             }
             else
             {
-                $header.="<a href='../../' class='brand-logo'>
-	        						<i class='material-icons'>web</i>
-	    						</a>";
+                $header.="<div class='navbar-header'>
+                            <button type='button' class='navbar-toggle' data-toggle='collapse' data-target='#bs-example-navbar-collapse-1'>
+                                <span class='sr-only'>Toggle navigation</span>
+                                <span class='icon-bar'></span>
+                                <span class='icon-bar'></span>
+                                <span class='icon-bar'></span>
+                            </button>
+                            <a class='navbar-brand' href='index.html'>Start Bootstrap</a>
+                        </div>
+                        <div class='collapse navbar-collapse' id='bs-example-navbar-collapse-1'>
+                <ul class='nav navbar-nav navbar-right'>
+                    <li>
+                        <a href='../main/login.php'>Volver al login</a>
+                    </li>
+                    <li class='dropdown'>
+                        <a href='#' class='dropdown-toggle' data-toggle='dropdown'><div class='chip'>
+                              <img src='data:image/*;base64,$_SESSION[img_admin]' alt='Person' width='40' height='40' class='img-circle'>
+                              $_SESSION[usuario_admin]
+                            </div> <b class='caret'></b></a>
+                        <ul class='dropdown-menu'>
+                            <li>
+                                <a href='../profile/'>Editar Perfil</a>
+                            </li>
+                            <li>
+                                <a href='../main/logout.php'>Salir</a>
+                            </li>
+                        </ul>
+                    </li>
+                </ul>
+            </div>";
             }
-            $header .= "</div>
-		    			</nav>
-	  				</div>
+            $header .= "
 	  				<div class='container center-align'>";
             print ($header);
             if($session)
@@ -252,7 +292,7 @@
             }
             else
             {
-                if($filename != "login.php" && $filename != "register.php" && $filename != "activesesion.php" && $filename != "404.php")
+                if($filename != "login.php" && $filename != "register.php" && $filename != "activesesion.php" && $filename != "404.php"&& $filename != "denied.php")
                 {
                     print("<div class='card-panel red'><a href='../main/login.php><h5>Debe iniciar sesion.</h5></a></div>");
                 }
@@ -262,3 +302,46 @@
                 }
             }
         }
+
+        public static function footer()
+        {
+            $footer="<script src="js/jquery.js"></script>
+
+            <!-- Bootstrap Core JavaScript -->
+            <script src="js/bootstrap.min.js"></script>
+
+            <!-- Script to Activate the Carousel -->
+            <script>
+            $('.carousel').carousel({
+                interval: 5000 //changes the speed
+            })
+            </script>
+
+        </body>
+
+        </html>";
+            print($footer);
+        }
+
+        public static function setCombo($name, $value, $query)
+        {
+            $data =Database::getRows($query,null);
+            $combo="<select name='$name' requeried>";
+            if($value == null)
+            {
+                $combo .= "<option value='' disabled selected>Selecione una opcion</option>";
+            }
+            foreach($data as $row)
+            {
+                $combo .="<option value='$row[0]'";
+                if(isset($_POST[$name])==$row[0] || $value == $row[0])
+                {
+                    $combo.="selected";
+                }
+                $combo .=">$row[1]</option>";
+            }
+            $combo.="</select>
+                    <label style='text-transform:capitalize;'>$name</label>";
+            print($combo);
+        }
+    }
