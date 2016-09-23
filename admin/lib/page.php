aab6d839b1c03204bee52884872c607ef8fad192
enriks <nelo.coto@gmail.com>
2016-09-23 17:12:00 -0600

    Subida Fer
    validaciones, estoy buscando mas validaciones para campos de precio y
    porcentaje

--- admin/lib/page.php
+++ admin/lib/page.php
@@ -23,7 +23,7 @@
 
                 <head>
 
-                    <meta charset='utf-8'>
+                    <meta charset='UTF-8'>
                     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
                     <meta name='viewport' content='width=device-width, initial-scale=1'>
                     <meta name='description' content=''>
@@ -75,7 +75,7 @@
 
                 <head>
 
-                    <meta charset='utf-8'>
+                    <meta charset='UTF_8'>
                     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
                     <meta name='viewport' content='width=device-width, initial-scale=1'>
                     <meta name='description' content=''>
